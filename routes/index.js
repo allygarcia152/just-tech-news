45fd0bd3a5e6881d5955e3342d9f06e6d1ef2886
AllyGarcia <allygarcia152@gmail.com>
2022-04-28 19:56:09 -0500

    add CRUD opperations

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
