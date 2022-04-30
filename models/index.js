5ed3eeab877a7a706d4447be35e697ac67ed9d4d
AllyGarcia <allygarcia152@gmail.com>
2022-04-30 15:28:13 -0500

    add update and delete to post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// post defines the post model, user defines the user model, index imports both and defines their link/connection/assossiation
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+//force: true basically works like DROP TABLE IF EXISTS it alows the table to be overwritten and re-created
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
