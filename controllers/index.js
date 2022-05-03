b9613c651f1a588aeae8689c5b7334939832f07e
AllyGarcia <allygarcia152@gmail.com>
2022-05-02 19:23:31 -0500

    create homepage including view folder and files as well as rename routes to controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connections');
+//make the stylesheet available to the client
+const path = require('path');
+//add app's template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//express.static is a built in express middleware function that can take all of the contents of a folder andserve them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 //force: true basically works like DROP TABLE IF EXISTS it alows the table to be overwritten and re-created
