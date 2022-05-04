818efbfc48f831d9d3f342bc3af50a0ad615352e
AllyGarcia <allygarcia152@gmail.com>
2022-05-03 19:25:48 -0500

    add login and logout ability to users via front facing js files in public folder

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connections');
 //make the stylesheet available to the client
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
 //add app's template engine
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//connect to the session that sequelize is using for the db
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,10 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(require('./controllers'));
 
 // turn on connection to db and server
 //force: true basically works like DROP TABLE IF EXISTS it alows the table to be overwritten and re-created
